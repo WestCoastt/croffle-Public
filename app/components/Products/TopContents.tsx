a5a0835515ad19b45913179b8d95584295ab3728
westcoast-dev <westcoast@swave.kr>
2023-06-16 16:44:39 +0900

    design(detail): add product options and shipping info

--- app/components/Products/TopContents.tsx
+++ app/components/Products/TopContents.tsx
@@ -55,6 +55,8 @@ const PriceContainer = styled.div`
 `;
 
 const PriceWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
   margin-bottom: 8px;
   font-size: 18px;
   font-weight: 700;
@@ -62,12 +64,105 @@ const PriceWrapper = styled.div`
     font-size: 32px;
   }
 `;
+const PriceBox = styled.div`
+  display: flex;
+  align-items: center;
+  .reg {
+    font-size: 16px;
+    font-weight: 400;
+    margin-right: 10px;
+    color: #999999;
+    text-decoration: line-through;
+  }
+  .rate {
+    font-weight: 700;
+    font-size: 24px;
+    color: var(--primary);
+  }
+`;
+
 const CoinWarpper = styled.div`
   color: var(--primary);
   font-size: 14px;
-  font-weight: 500;
+  font-weight: 700;
   span {
-    font-size: 20x;
+    font-size: 20px;
+  }
+`;
+
+const ShippingContainer = styled.div`
+  width: 300px;
+  margin-top: 20px;
+  font-size: 14px;
+
+  .shipping_info {
+    span:first-child {
+      margin-right: 38px;
+    }
+  }
+
+  .shipping_fee {
+    margin-top: 10px;
+
+    span:first-child {
+      margin-right: 52px;
+    }
+  }
+`;
+
+const SelectBox = styled.div<{ dropdown: boolean }>`
+  position: relative;
+  width: 100%;
+  margin-top: 24px;
+  font-size: 14px;
+  cursor: pointer;
+
+  .select {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 100%;
+    height: 48px;
+    padding: 0 18px;
+    border: 1px solid #000;
+    border-radius: 5px;
+    border-bottom-left-radius: ${(props) => props.dropdown && "0"};
+    border-bottom-right-radius: ${(props) => props.dropdown && "0"};
+
+    span {
+      width: 470px;
+      height: 20px;
+      overflow: hidden;
+    }
+  }
+
+  .list {
+    position: absolute;
+    width: 100%;
+    max-height: 196px;
+    margin: 0;
+    top: 46px;
+    left: 0;
+    right: 0;
+    z-index: 10;
+    padding: 0;
+    list-style: none;
+    background: #fff;
+    border: 1px solid #000;
+    border-bottom-left-radius: 5px;
+    border-bottom-right-radius: 5px;
+    overflow-y: scroll;
+    list-style: none;
+
+    li {
+      padding: 14px 18px;
+      &:hover {
+        background-color: #f6f7f9;
+      }
+    }
+  }
+  .up {
+    transform: rotate(180deg);
   }
 `;
 
@@ -87,10 +182,33 @@ const detail = {
   stars: 3.4,
   reviews: 6178,
   src: "https://github.com/westcoast-dev/nextjs-course/assets/117972001/fde3989f-bc08-4909-8298-ed4322be612d",
+  shipping_fee: 3000,
+  estimated_time: "Sat Jul 1 2023 10:23:29 GMT+0900",
+  option: [
+    "옵션1. 훌라훌라 훌라춤을 춘다 탬버린 비누",
+    "옵션2. 손 세정제",
+    "옵션3. 탬버린즈 버블버블 액션빔",
+    "옵션4. 핸드크림",
+    "옵션5. 딥디크 시그니엘 시그니쳐 바디로션",
+    "옵션6. 짱구는 못말려 부리부리부리부리 대마왕의 자라나라 머리머리 머머리 탈모 방지 샴푸",
+  ],
 };
+
+const days = ["일", "월", "화", "수", "목", "금", "토"];
+const date = new Date(detail.estimated_time).toLocaleString().split(". ");
+const day = new Date(detail.estimated_time).getDay();
+const eta = `${date[1] + "/" + date[2]}` + `(${days[day]})`;
 
 export default function TopContents() {
   const [mainImg, setMainImg] = useState(images[0]);
+  const [selected, setSelected] = useState("선택하세요.");
+  const [dropdown, setDropdown] = useState(false);
+
+  const discount_rate = Math.floor(
+    (1 - detail.total_price / detail.regular_price) * 100
+  );
+
+  console.log(eta);
 
   return (
     <Container>
@@ -117,12 +235,70 @@ export default function TopContents() {
         <Rating stars={detail.stars} reviews={detail.reviews} />
         <PriceContainer>
           <PriceWrapper>
-            <span>{detail.total_price.toLocaleString()}</span>원
+            <div>
+              <span>{detail.total_price.toLocaleString()}</span>원
+            </div>
+            {detail.regular_price && (
+              <PriceBox>
+                <span className="reg">
+                  {detail.regular_price.toLocaleString()}원
+                </span>
+                <span className="rate">{discount_rate}%</span>
+              </PriceBox>
+            )}
           </PriceWrapper>
           <CoinWarpper>
             <span>{(detail.total_price / 1000).toLocaleString()}</span>Croffle
           </CoinWarpper>
         </PriceContainer>
+        <ShippingContainer>
+          <div className="shipping_info">
+            <span>배송정보</span>
+            <span>
+              <strong>택배배송</strong> {eta} 도착예정
+            </span>
+          </div>
+          <div className="shipping_fee">
+            <span>배송비</span>
+            <strong>
+              {detail.shipping_fee === 0
+                ? "무료"
+                : detail.shipping_fee.toLocaleString() + "원"}
+            </strong>
+          </div>
+        </ShippingContainer>
+
+        <SelectBox dropdown={dropdown}>
+          <div
+            className="select"
+            onClick={() => {
+              setDropdown(!dropdown);
+            }}
+          >
+            <span>{selected}</span>
+            <img
+              className={dropdown ? "up" : ""}
+              src="/assets/img/option_arrow.svg"
+              alt="category-select"
+            />
+          </div>
+
+          {dropdown && (
+            <ul className="list">
+              {detail.option.map((item) => (
+                <li
+                  key={item}
+                  onClick={() => {
+                    setSelected(item);
+                    setDropdown(false);
+                  }}
+                >
+                  {item}
+                </li>
+              ))}
+            </ul>
+          )}
+        </SelectBox>
       </InfoContainer>
     </Container>
   );
